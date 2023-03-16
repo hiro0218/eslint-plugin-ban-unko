a69e0bb64800aa3e3547ef3ddff1644c35043440
hiro0218 <hiro0218@gmail.com>
2023-03-17 00:53:11 +0900

    fix

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,82 +1,30 @@
-"use strict";
-
 const { RuleTester } = require("eslint");
-const plugin = require("../lib");
+const rule = require("../lib/rules/no-unko").rules["no-unko"];
 
-const ruleTester = new RuleTester({
-  parserOptions: {
-    ecmaVersion: 2021
-  }
-});
-
-ruleTester.run("no-unko", plugin.rules["no-unko"], {
-  valid: [
-    'const helloWorld = "Hello World";',
-    'const noUnko = "noUnko";'
-  ],
+const ruleTester = new RuleTester();
+ruleTester.run("no-unko", rule, {
+  valid: ['"hello"', '"HELLO"', "console.log(123)", { code: "var hoge = 1" }],
   invalid: [
-    {
-      code: 'const unko = "💩";',
-      errors: [
-        {
-          message: "The word '💩' is banned",
-          type: "Literal"
-        }
-      ]
-    },
-    {
-      code: 'const message = "I stepped on unko";',
-      errors: [
-        {
-          message: "The word 'unko' is banned",
-          type: "Literal"
-        }
-      ]
-    },
-    {
-      code: "const message = `I saw a pile of うんこ yesterday`;",
-      errors: [
-        {
-          message: "The word 'うんこ' is banned",
-          type: "TemplateElement"
-        }
-      ]
-    },
-    {
-      code: "const message = `I smelled ｳﾝｺ on the street`;",
-      errors: [
-        {
-          message: "The word 'ｳﾝｺ' is banned",
-          type: "TemplateElement"
-        }
-      ]
-    },
-    {
-      code: "const message = `My dog rolled in ウンコ at the park`;",
-      errors: [
-        {
-          message: "The word 'ウンコ' is banned",
-          type: "TemplateElement"
-        }
-      ]
-    },
-    {
-      code: "const message = `The poop emoji is my favorite emoji`;",
-      errors: [
-        {
-          message: "The word 'poop' is banned",
-          type: "TemplateElement"
-        }
-      ]
-    },
-    {
-      code: 'const message = `I prefer to use the word "feces" instead of "unko"`;',
-      errors: [
-        {
-          message: "The word 'feces' is banned",
-          type: "TemplateElement"
-        }
-      ]
-    }
-  ]
+    invalid('"💩"'),
+    invalid('"うんこ"'),
+    invalid('"ｳﾝｺ"'),
+    invalid('"ウンコ"'),
+    invalid('"unko"'),
+    invalid('"poop"'),
+    invalid('"feces"'),
+    invalid('var foo = "unko"'),
+    invalid('var unko = "foo"'),
+    invalid("function unko() {}"),
+  ],
 });
+
+function invalid(code, output) {
+  const invalidTest = {
+    code,
+    errors: [{ message: "The use of strings similar to 💩 is prohibited." }],
+  };
+  if (output) {
+    invalidTest.output = output;
+  }
+  return invalidTest;
+}
